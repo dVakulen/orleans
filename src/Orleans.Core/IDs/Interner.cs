ec65239864ca27c1178f899882ff5730d9e0a83c
Dmitry Vakulenko <dmitry.vakulenko@outlook.com>
2017-11-02 17:42:17 +0200

    Some thoughts

--- src/Orleans.Core/IDs/Interner.cs
+++ src/Orleans.Core/IDs/Interner.cs
@@ -1,4 +1,138 @@
-using System;
+﻿Orleans. Current and vNext.
+
+Orleans have to complete with raw in memory dicti0nary with fine granularity across farms , thus ensuring local consistency  -
+// orleans provides good model with sometimes unacceptable performance implications
+-- movingffrom 1 to 2 machines - 2x perf drop. _ There should not be it. Also this drop essentially means that  multi-node cluster performs at best at 50% of its capability, (grain directory cache_)
+
+chineses in repo with strange issues
+//  e.g. orleans already built bbetween clients and db ( asp.net mvc with in memory cache with ttl expiration policies )  - really simple simplicity, har
+
+
+
+
+
+Widen orleans community
+
+assumptions: 
+		- gaining community reach is of priority for the team
+	- distrivuted actors is future of cloud computatios (taking most optimistic branch) 
+	-- yes, the idea is nice, but we need people to use it. 
+Statements about of asp.net mvc integration should be main focus 
+
+- assuming that gaining tracion is of any importance to current directive
+- asp.net mvc integration should be main focus 
+= (this on is rather gihht priority) spend team effort to fit Orleans into https://github.com/dotnet-architecture/eShopOnContainers (Orleans team have some weight compared to ousider conbvutor) as it being main example of mvc design for whole .net community
+
+- architecture slide with fit into default mvc model is needed as example (mvc + EF)
+- mvc focus should be empatised as being main (this is assumption - move to assumptions)  need of users which look at orleans
+
+ - benefist should be stated from perspective of aveage web dev, with comparison to cache
+
+
+  - http://dotnet.github.io/orleans/Documentation/Introduction.html - great in terms of absrtactions, but another view with abstacrtions concretized as asp mvc should be next to it.  - action item - team, check whats really needs to be done
+
+
+- currenlty mvc interactions is under some cryptic http://dotnet.github.io/orleans/Tutorials/Front-Ends-for-Orleans-Services.html 
+It couldn;t be even seen from left menu http://dotnet.github.io/orleans/index.html, requiring navigation to tutorials? - is it a problem?
+
+- there's no clear 	obstacles on path of making Orleans just reasonably slower than in memory dictionary access insted of being orders of magnitudes slower
+for it to become standart - it have some great perf implications, currently reasoned as price for distribution, but this price can be turned into pay for what use
+
+- actions: 
+- make single host usage cases as optimized as possible ( also colocating with client in same process + dynamic actor repartition with aggresively optimizedSingleHostCase should result in both throughput and latency being comparable to in-memory dictionary access)
+
+ - price for distribution should not be payed upfront -  perf gains from move from A to B nodes count should reasebmle the ones from 0 to A nodes
+
+ - from prysm of asp mvc user
+ - users neither dont need nor want to pay the costs of Orleans upfront, as most users's  cases being solved with simple caches with,
+
+ distributed part of orleans is usually being unused , as most users dont have such (>1 node) scale.
+ \
+  - create 2 prism of view, extremums - stantard asp.mvc TDefaultMvc
+   - high scale app. THighScale
+  
+  - ACores MMemory currenlty giving single Orleans server 2x cpus and mem - without client app 
+  bottlenecks should result in ~2x perf increase and to be ~ 3x more perfomant than 2 nodes with half of cpus (due to IO + serialization costs),but its not due to locks  
+  
+ And the ones who have - don't want to pay the price.
+ . The ones 
+ 
+  
+  
+  -- main theme - orleans as future of distributed computing, and integrational part of .Net framework
+  
+   - prysm of 95% use cases - model of cache is nice, though costs..
+   - prysm of high scale remainign ones - they usually have power to hand craft something similar on top of Akka in order not to pay the costs.
+   
+   -- Akka is strong competitor , wih only lacking one feature  - seamless fit into languaqe model. 
+   (Also, if performance was comparable with Akka's - there could be some thougths on joing efforts and creating unified model for .Net ecosystem.
+  - perf related question -  Maybe bringing orleans model to akka with clustering would be easier effort than optimizing Orleans) 
+   )
+   
+	 
+	  -- Akka.Net does not bring costs, being almost as performant as Dataflow blocks\ hand coded threading - laks language fit and Microsoft's power behind
+	  
+	  
+   - microsevises for .Net - https://github.com/gigya/microdot - it's success to some certain depends on Orleans perf
+   
+    - -- user standpoint: I want my objects to be highly scalabe, but still look alike ordinary .Net objects
+	
+	
+	 -- some slight efforts(- - threading rework reference) resulted in up to 89% perf increase, making Orleans remotely comparable to Akka.Net in terms of single host execution thgoughput 
+	 
+	 
+	 
+	 
+	 - assumption - everithing is ASP.MVC
+	 
+	 - focus on out of box expirience - asp net dev should be able to use Orleans after package installation, without any preconfiguration
+	 
+	  - head of PR
+	  Orleans nodel while theoretically fitting nicely into  95% mvc users cases - at practice neither clearly states how to do that, nor provides acceptable perf.
+	  
+	  - MS can not currently state that Orleans is way to go and PR Orleans alongside the MVC due to it's perf implications
+	  - single host in process perf - critical.
+	  - in order to gain traction and get uses - following  could be done: .....
+     - introduce notion of akkas  distributed data (https://doc.akka.io/docs/akka/2.5.4/scala/distributed-data.html)
+	 - with view of (Dictionary<K,V> ) - basically a cache - as default in mem caching for distributed apps  - with distributed transactions should be easy
+ --  yhat cache should be quite configurable in order to cover cases from InMemoryDict to multicluster wide cache ]	   (some inspiration from https://hazelcast.com/use-cases/caching/)
+ 
+ 
+ 
+	 - convinient streams DSL, Orleans should handle that part, basically becoming distributed DataFlow. as 95% of workload have static dataflow processing plans, thus leading to success of tools with ablility to heavily optimize this kind of plans,
+	 such as Spark.   Dynamically reconfigurable streams could also be covered by that DSL.  -
+	 -
+	 --- orthogonal, but nice to have geatures: Distr data, streams DSL. (with reasonings and real life examples ))
+	 
+	 - in current world of Distributed multicore processors MS has to offer default  solution to be used on premises for this matter similar on how the ThreadPool for node cores (Orleans for nodes as .Net TaskScheduler for cores)
+	  -- average use case req - take mvc app in vacuum,  analyze - for Orleans, Distr data, Streams DSl. -(expandable part OF Pr )
+	  - the main rev - perf.
+	  -- plan - become a future (de-facto in .Net world)(
+	  add reasoing  - simplicity of use, whith uncopromised at low scale performance, i).
+
+	  requirements - Asp.MVC focus, PR from Microsoft wit and performance.
+	  
+	   -- it should be as easy to use as ordinary cahce ( it already is) 
+	   - it shouldn't bring pow(10, n) perf drop. - thats the problem part
+	      -- become part of channels api
+	    - if that sounds good to team - I'll take some actions?
+
+		
+		- Orleans team, could you please share your thoughts on this matter? - as separate comment
+		
+		 == is it possible to have MS' greater support after perf increase?  - direct message after PR
+		 
+		  -- recenlty encountered just another case of orleans fit
+		  
+					
+ - problem: .Net lacks supported by MS cloud independent solution for cluster processing.
+  - most obvious candidate could be guessed pretty easily by starring at https://github.com/dotnet
+ - though stars count is high - actual usage which can be seen at nuget downloads is two times lower than Akka.Net counterpart. - Community likes the idea,  but contunues to use Akka \ in house solutions due to Orleans' abstractions costs. This costs can be removed.
+		- all it to be true needs just some belief in Orleans
+		
+		 assuming that Asp.net mvc developer is the largest part current .Net community
+		--everythung that is going to be said boils down to simple questions: why Asp.net mvc developer doesn't use Orleans and how to fix it.
+		using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
