df66d11bd76db4e0047bbf193e0928a7a4ab5f46
Dmitry Vakulenko <dmitry.vakulenko@outlook.com>
2017-11-09 20:44:43 +0200

    +

--- src/Orleans.Core/Runtime/AsynchAgent.cs
+++ src/Orleans.Core/Runtime/AsynchAgent.cs
@@ -99,8 +99,30 @@ namespace Orleans.Runtime
     // due to number of entities to take new dependency for reducing churn
     // it's order in parameters list will be in case of presense of log factory  - right before it, 
     // not accounting it's importance to class functioning (for churn reducing)
-
-    //  class StageExecutionHandle
+    /**
+   * Submits a value-returning task for execution and returns a
+   * Future representing the pending results of the task. The
+   * Future's {@code get} method will return the task's result upon
+   * successful completion.
+   *
+   * <p>
+   * If you would like to immediately block waiting
+   * for a task, you can use constructions of the form
+   * {@code result = exec.submit(aCallable).get();}
+   *
+   * <p>Note: The {@link Executors} class includes a set of methods
+   * that can convert some other common closure-like objects,
+   * for example, {@link java.security.PrivilegedAction} to
+   * {@link Callable} form so they can be submitted.
+   *
+   * @param task the task to submit
+   * @param <T> the type of the task's result
+   * @return a Future representing pending completion of the task
+   * @throws RejectedExecutionException if the task cannot be
+   *         scheduled for execution
+   * @throws NullPointerException if the task is null
+   */
+    //  <T> Future<T> submit(Callable<T> task); (ref - java ExecutorService)
     abstract class ExecutorService // not needed?  .. stage info requirement is leaking from below .. 
     {
         // returns stageExecutionHandle // agent.stop  used only for cts, handle not needed? 
