a7ed5a78d3ba17f213d57016f61532437cb94f0c
Dmitry Vakulenko <dmitry.vakulenko@outlook.com>
2017-11-10 01:01:01 +0200

    to be reverted

--- test/TestGrains/MultipleGenericParameterInterfaceImpl.cs
+++ test/TestGrains/MultipleGenericParameterInterfaceImpl.cs
@@ -7,22 +7,22 @@ using System.Threading.Tasks;
 
 namespace UnitTests.Grains
 {
-    interface IFactory<TInput, TOutput>
-    {
-        Task<TOutput> Product(TInput input);
-    }
+    //interface IFactory<TInput, TOutput>
+    //{
+    //    Task<TOutput> Product(TInput input);
+    //}
 
-    class Factory
-    {
-        public int SomeProperty { get; set; }
-    }
+    //class Factory
+    //{
+    //    public int SomeProperty { get; set; }
+    //}
 
-    class AFactory : Factory, IFactory<int, double>
-    {
-        async Task<double> IFactory<int, double>.Product(int input)
-        {
-            await Task.Delay(100);
-            return input;
-        }
-    }
+    //class AFactory : Factory, IFactory<int, double>
+    //{
+    //    async Task<double> IFactory<int, double>.Product(int input)
+    //    {
+    //        await Task.Delay(100);
+    //        return input;
+    //    }
+    //}
 }
